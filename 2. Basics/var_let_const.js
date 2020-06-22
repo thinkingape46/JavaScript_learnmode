13f42ed9f13ed3263e0c196a5be2a38e9ba9350e
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-06-22 22:30:03 +0530

    Create var_let_const.js

---
+++ 2. Basics/var_let_const.js
@@ -0,0 +1,41 @@
+// // Define a variable with 'var'
+
+// var name = 'John Doe';
+
+// // Console log the name
+
+// console.log(name)
+
+// // Intilizing a variable for future use.
+
+// var greeting;
+
+// // naming convention for variables
+
+// // Cannot start with a number
+// // Can start with "$", use with jQuery
+// // Underscore can be used. used for private variables.
+
+// // conventions for multi word variables.
+
+// // Use came case
+// var firstName = 'John'
+
+// // class can start with upper case e.g FirstName
+
+
+// LET VARIABLE
+
+// let is identical in terms of global scope, but it has advantage in block level
+
+// CONST VARIABLE
+
+// Stands for constant, IT CANNOT BE REASSIGNED.
+const name = 'John Joe'
+
+const name;
+
+// CONST CANNOT BE INITIALIZED EMPTY
+
+// when CONST is used to define a JS Object, value for a key can still be changed, but we cannot reassign it something else.
+
