26d50581da0979ffa06dc53dea899a1e3ed3788c
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-07-01 23:14:26 +0530

    Update ten_days.js

--- Brad_Schiff/ten_days.js
+++ Brad_Schiff/ten_days.js
@@ -17,29 +17,109 @@
 
 //   filter function in JavaScript
 
-let pets = [
-    {name: "Tracy", species: "cat", age: 1},
-    {name: "Camper", species: "dog", age: 2},
-    {name: "Brits", species: "dog", age: 5}
-  ]
+// let pets = [
+//     {name: "Tracy", species: "cat", age: 1},
+//     {name: "Camper", species: "dog", age: 2},
+//     {name: "Brits", species: "dog", age: 5}
+//   ]
 
-let onlyDogs = pets.filter(filterDogs)
-// filter(filterDogs) returns an array of dogs, since this is an array and filter function is still available.
-// We add another filter function i.e. filterBabies.
-// Chain of filter and map methods.
-let onlyBabies = pets.filter(filterDogs).filter(filterBabies).map(onlyNames)
+// let onlyDogs = pets.filter(filterDogs)
+// // filter(filterDogs) returns an array of dogs, since this is an array and filter function is still available.
+// // We add another filter function i.e. filterBabies.
+// // Chain of filter and map methods.
+// let onlyBabies = pets.filter(filterDogs).filter(filterBabies).map(onlyNames)
 
-function filterDogs(x) {
-    return x.species == "dog"
+// function filterDogs(x) {
+//     return x.species == "dog"
+// }
+
+// function filterBabies(x) {
+//     return x.age < 5
+// }
+
+// function onlyNames(x) {
+//     return x.name
+// }
+
+// console.log(onlyDogs)
+// console.log(onlyBabies)
+
+
+// ################# DAY 8 ################### //
+// ###### VARIABLE SCOPE AND CONTEXT ######## //
+
+// let myName = "Brad"
+
+console.log("------------------------VARIABLE SCOPE-----------------------")
+
+// function sayName() {
+//     let myName = "Jack"
+//     console.log(myName)    
+// }
+
+// sayName()
+// console.log(myName)
+
+// function hello() {
+//     let myName = "Jack"
+//     return myName
+// }
+
+// // console.log(myName)
+// console.log(hello())
+// console.log(myName)
+
+// Changing the global variable
+
+// console.log("Global Scope", myName)
+
+// function hello() {
+//     myName = "Jack"
+// }
+
+// hello()
+// console.log("Global Scope", myName)
+
+// VAR and LET variables.
+// LET - block scope
+// VAR - function scope
+
+// function hello() {
+//     // let myName = "function level block"
+//     if  (1 == 1) {
+//         var myName = "if level block"
+//         console.log(myName)
+//     }
+//     console.log(myName)
+// }
+
+// hello()
+// console.log(myName)
+
+
+console.log("-----------------------VARIABLE CONTEXT-----------------------")
+
+let name = {
+    firstName: "John",
+    lastName: "Doe",
+    drive() {
+        function someRandom() {
+            console.log(this)
+        }
+        someRandom()
+        console.log(this.firstName + " " + this.lastName + " is driving.")
+    }
 }
 
-function filterBabies(x) {
-    return x.age < 5
+console.log(name.drive())
+
+console.log("the 'this' keyword points towards the object that is executing the function")
+console.log(this)
+
+function breathe(x) {
+    console.log(this.firstName + " " + this.lastName + " inhaled and exhaled.")
 }
 
-function onlyNames(x) {
-    return x.name
-}
+console.log("-------Below line use 'call' method-------------")
 
-console.log(onlyDogs)
-console.log(onlyBabies)
+breathe.call(name)
