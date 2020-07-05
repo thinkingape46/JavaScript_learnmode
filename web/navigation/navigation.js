2251bcddbe90a3c6b021689007b57761041e72a7
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-07-05 23:25:05 +0530

    navigation bar

---
+++ web/navigation/navigation.js
@@ -0,0 +1,10 @@
+dD = document.getElementById("dropDown")
+dDlist = document.getElementById("dropDownList")
+
+dD.onmouseover = function() {
+    dDlist.style.display = "block";
+}
+dD.onmouseout = function() {
+    dDlist.style.display = "none";
+    
+}
