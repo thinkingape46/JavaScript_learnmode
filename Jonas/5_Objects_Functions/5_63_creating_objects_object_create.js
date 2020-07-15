a479c8208ae7e41e8709be225bdd5b6550c880b1
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-07-15 15:49:36 +0530

    Object.create method.

---
+++ Jonas/5_Objects_Functions/5_63_creating_objects_object_create.js
@@ -0,0 +1,20 @@
+/*
+// Object.create method
+*/
+
+var personProto = {
+    calculateAge: function() {
+        console.log(2020 - this.yearOfBirth);
+    }
+};
+
+var john = Object.create(personProto);
+john.name = "John";
+john.dateofBirth = 1990;
+john.job = "teacher";
+
+var jane = Object.create(personProto, {
+    name: {value: "Jane"},
+    yearOfBirth: {value: 1980},
+    job: {value: "designer"}
+})
